12604e70e5275a4b2d285682a856da4501c33842
BorislavChernev <borocool359@gmail.com>
2023-03-02 23:12:47 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/subqueries-and-joins/03-Sales-Employees.sql
@@ -0,0 +1,6 @@
+--03. Sales Employees
+SELECT TOP (50) e.EmployeeID, e.FirstName, e.LastName, d.Name AS DepartmentName
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE d.Name = 'Sales'
+ORDER BY EmployeeID
