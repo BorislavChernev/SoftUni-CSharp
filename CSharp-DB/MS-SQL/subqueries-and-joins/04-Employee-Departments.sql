7ca5af19fbc5b1ec327b768b485cf7424d3c24bf
BorislavChernev <borocool359@gmail.com>
2023-03-04 11:37:11 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/subqueries-and-joins/04-Employee-Departments.sql
@@ -0,0 +1,6 @@
+--04. Employee Departments
+SELECT TOP (5) e.EmployeeID, e.FirstName, e.Salary, d.Name
+FROM Employees AS e
+JOIN Departments AS d ON e.DepartmentID = d.DepartmentID
+WHERE Salary > 15000
+ORDER BY d.DepartmentID
