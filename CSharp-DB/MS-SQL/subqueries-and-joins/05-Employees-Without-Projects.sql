c4077cd1d03b4a32344d91fb8506518cd2df8cea
BorislavChernev <borocool359@gmail.com>
2023-03-04 11:37:16 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/subqueries-and-joins/05-Employees-Without-Projects.sql
@@ -0,0 +1,6 @@
+--05. Employees Without Projects
+SELECT TOP (3) e.EmployeeID, e.FirstName
+FROM Employees AS e
+LEFT JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+WHERE ep.ProjectID IS NULL
+ORDER BY EmployeeID
