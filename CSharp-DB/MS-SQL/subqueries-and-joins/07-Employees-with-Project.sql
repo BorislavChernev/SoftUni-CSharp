45dd9b55a00cb71f0b5f41558c9031f93dc6aa1e
BorislavChernev <borocool359@gmail.com>
2023-03-04 11:37:32 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/subqueries-and-joins/07-Employees-with-Project.sql
@@ -0,0 +1,7 @@
+--07. Employees with Project
+SELECT TOP (5) e.EmployeeID, e.FirstName, p.Name AS [ProjectName]
+FROM Employees AS e
+JOIN EmployeesProjects AS ep ON e.EmployeeID = ep.EmployeeID
+JOIN Projects AS p ON ep.ProjectID = p.ProjectID
+WHERE p.StartDate > '2002/08/13' AND p.EndDate IS NULL
+ORDER BY e.EmployeeID
