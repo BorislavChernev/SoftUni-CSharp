072c21a2bd4a27f77c64ccef00f470eda9a0d829
BorislavChernev <borocool359@gmail.com>
2023-03-07 22:47:01 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/built-in-functions/11-Find-All-Employees-with-Rank-2.sql
@@ -0,0 +1,6 @@
+--11. Find All Employees with Rank 2
+SELECT * FROM (SELECT EmployeeID, FirstName, LastName, Salary, DENSE_RANK() OVER (PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000) AS MyTable
+WHERE RANK = 2
+ORDER BY Salary DESC
