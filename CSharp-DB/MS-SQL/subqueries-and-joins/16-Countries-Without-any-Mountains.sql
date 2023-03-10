6b6d143e8c6a85663d17135c103a1ca390ff6935
BorislavChernev <borocool359@gmail.com>
2023-03-10 23:04:19 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/subqueries-and-joins/16-Countries-Without-any-Mountains.sql
@@ -0,0 +1,5 @@
+--16. Countries Without any Mountains
+SELECT COUNT(c.CountryCode) AS Count
+FROM Countries AS c
+LEFT JOIN MountainsCountries AS mc ON c.CountryCode = mc.CountryCode
+WHERE mc.MountainId IS NULL
