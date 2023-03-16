ae646a6eefc94efb09b0b870f8b1b7298e6d4ae8
BorislavChernev <borocool359@gmail.com>
2023-03-16 23:07:05 +0200

    Completed Task

---
+++ CSharp-DB/MS-SQL/Indeces-and-agregiation/11-Average-Interest.sql
@@ -0,0 +1,6 @@
+--11. Average Interest
+SELECT DepositGroup, IsDepositExpired, AVG(DepositInterest) AS [AverageInterest]
+FROM WizzardDeposits
+WHERE DepositStartDate > '01/01/1985'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
