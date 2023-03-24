08dc518288b18ed517b04c82f631d5ba9ef43688
BorislavChernev <borocool359@gmail.com>
2023-03-24 22:19:49 +0200

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/Advanced-Querying/Exercises/BookShop.Models/Author.cs
@@ -0,0 +1,19 @@
+﻿namespace BookShop.Models;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+public class Author
+{
+    public Author()
+    {
+        this.Books = new HashSet<Book>();
+    }
+
+    public int AuthorId { get; set; }
+
+    public string FirstName { get; set; }
+
+    public string LastName { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; }
+}
