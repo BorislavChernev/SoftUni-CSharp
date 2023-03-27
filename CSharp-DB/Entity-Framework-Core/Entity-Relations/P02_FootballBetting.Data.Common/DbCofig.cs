5c7adce7b03662aaf4b57ff781dbae5ba63514b4
BorislavChernev <borocool359@gmail.com>
2023-03-27 16:38:45 +0300

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/Entity-Relations/P02_FootballBetting.Data.Common/DbCofig.cs
@@ -0,0 +1,7 @@
+﻿namespace P02_FootballBetting.Data.Common;
+
+public class DbConfig
+{
+    public const string ConnectionString =
+        @"Server=.;Database=Bet388;Integrated Security=True;Encrypt=False;";
+}
