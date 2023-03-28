4744a48a4da1a5e88ebbca93ebc3cea4fa0be4b4
BorislavChernev <borocool359@gmail.com>
2023-03-28 20:54:54 +0300

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/LINQ/MusicHub/Data/MusicHubDbContext.cs
@@ -0,0 +1,58 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Album> Albums { get; set; }
+
+        public DbSet<Performer> performers { get; set; }
+
+        public DbSet<Producer> Producers { get; set; }
+
+        public DbSet<Song> Songs { get; set; }
+
+        public DbSet<SongPerformer> songPerformers { get; set; }
+
+        public DbSet<Writer> writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Song>(entity =>
+            {
+                entity
+                .Property(s => s.CreatedOn)
+                .HasColumnType("date");
+            });
+
+            builder.Entity<SongPerformer>(entity =>
+            {
+                entity
+                    .HasKey(sp => new
+                    {
+                        sp.SongId,
+                        sp.PerformerId,
+                    });
+            });
+        }
+    }
+}
