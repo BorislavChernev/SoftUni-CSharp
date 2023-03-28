574f6bb35da0c0be18a1697d125db6c6ee9463b2
BorislavChernev <borocool359@gmail.com>
2023-03-28 20:54:58 +0300

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/LINQ/MusicHub/Data/Models/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicHub.Data.Models;
+
+public class Writer
+{
+    [Key]
+    public int Id { get; set; }
+
+    [MaxLength(ValidationConstants.WriterNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    public string? Pseudonym { get; set; }
+
+    public virtual ICollection<Song>? Songs { get; set; }
+}
