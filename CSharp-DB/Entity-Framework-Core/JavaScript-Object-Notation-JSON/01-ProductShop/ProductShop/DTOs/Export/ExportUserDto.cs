41850706e36ad26563211713222b896ea0c0c219
BorislavChernev <borocool359@gmail.com>
2023-04-03 19:43:38 +0300

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-ProductShop/ProductShop/DTOs/Export/ExportUserDto.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace ProductShop.DTOs.Export;
+
+public class ExportUserDto
+{
+    [JsonProperty("firstName")]
+    public string? FirstName { get; set; }
+
+    [JsonProperty("lastName")]
+    public string LastName { get; set; } = null!;
+
+    [JsonProperty("age")]
+    public int? Age { get; set; }
+}
