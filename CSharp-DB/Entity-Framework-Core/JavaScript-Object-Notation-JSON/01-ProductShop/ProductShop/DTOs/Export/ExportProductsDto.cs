0f42973c54398ab7d0e7f02af4360e6aeae00593
BorislavChernev <borocool359@gmail.com>
2023-04-03 19:43:41 +0300

    Completed Task

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-ProductShop/ProductShop/DTOs/Export/ExportProductsDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace ProductShop.DTOs.Export;
+
+public class ExportProductsDto
+{
+    public ExportProductsDto()
+    {
+        this.ProductsSold = new HashSet<Product>();
+    }
+    [JsonProperty("firstName")]
+    public string? FirstName { get; set; }
+
+    [JsonProperty("lastName")]
+    public string LastName { get; set; } = null!;
+
+    [JsonProperty("soldProducts")]
+    public virtual ICollection<Product> ProductsSold { get; set; }
+}
