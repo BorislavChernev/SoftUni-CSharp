2894c331d18b0fe33701c389d4457bc401af7469
BorislavChernev <borocool359@gmail.com>
2023-04-14 22:46:04 +0300

    Create Player.cs

---
+++ CSharp-DB/Entity-Framework-Core/Entity-Relations/P02_FootballBetting.Data.Models/Player.cs
@@ -0,0 +1,37 @@
+﻿namespace P02_FootballBetting.Data.Models;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using Common;
+
+public class Player
+{
+    public Player()
+    {
+        this.PlayersStatistics = new HashSet<PlayerStatistic>();
+    }
+
+    [Key]
+    public int PlayerId { get; set; }
+
+    [Required]
+    [MaxLength(ValidationConstants.PlayerNameMaxLength)]
+    public string Name { get; set; } = null!;
+
+    public int SquadNumber { get; set; }
+
+    public bool IsInjured { get; set; }
+
+    [ForeignKey(nameof(Team))]
+    public int TeamId { get; set; }
+
+    public virtual Team Team { get; set; } = null!;
+
+    [ForeignKey(nameof(Position))]
+    public int PositionId { get; set; }
+
+    public virtual Position Position { get; set; } = null!;
+
+    public virtual ICollection<PlayerStatistic> PlayersStatistics { get; set; }
+}
