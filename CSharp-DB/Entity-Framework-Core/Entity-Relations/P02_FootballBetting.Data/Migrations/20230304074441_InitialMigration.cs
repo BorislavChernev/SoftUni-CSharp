9c9b0a87dda6fb44270f85d0b6ef6fe193ee5616
BorislavChernev <borocool359@gmail.com>
2023-04-20 17:58:41 +0300

    Create 20230304074441_InitialMigration.cs

---
+++ CSharp-DB/Entity-Framework-Core/Entity-Relations/P02_FootballBetting.Data/Migrations/20230304074441_InitialMigration.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P02_FootballBetting.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_TeamId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_TeamId1",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_TeamId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_TeamId1",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "TeamId1",
+                table: "Games");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_AwayTeamId",
+                table: "Games",
+                column: "AwayTeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_HomeTeamId",
+                table: "Games",
+                column: "HomeTeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_AwayTeamId",
+                table: "Games",
+                column: "AwayTeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_HomeTeamId",
+                table: "Games",
+                column: "HomeTeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_AwayTeamId",
+                table: "Games");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Teams_HomeTeamId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_AwayTeamId",
+                table: "Games");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_HomeTeamId",
+                table: "Games");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId",
+                table: "Games",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TeamId1",
+                table: "Games",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_TeamId",
+                table: "Games",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_TeamId1",
+                table: "Games",
+                column: "TeamId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_TeamId",
+                table: "Games",
+                column: "TeamId",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Teams_TeamId1",
+                table: "Games",
+                column: "TeamId1",
+                principalTable: "Teams",
+                principalColumn: "TeamId");
+        }
+    }
+}
