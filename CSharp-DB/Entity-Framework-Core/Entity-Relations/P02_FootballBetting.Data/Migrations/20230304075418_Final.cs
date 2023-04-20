a36c7515bcf4824da7b9cbed60f10d206fbfed30
BorislavChernev <borocool359@gmail.com>
2023-04-20 17:58:48 +0300

    Create 20230304075418_Final.cs

---
+++ CSharp-DB/Entity-Framework-Core/Entity-Relations/P02_FootballBetting.Data/Migrations/20230304075418_Final.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P02_FootballBetting.Data.Migrations
+{
+    public partial class Final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "MinutesPlayed",
+                table: "PlayersStatistics",
+                type: "tinyint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "MinutesPlayed",
+                table: "PlayersStatistics",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "tinyint");
+        }
+    }
+}
