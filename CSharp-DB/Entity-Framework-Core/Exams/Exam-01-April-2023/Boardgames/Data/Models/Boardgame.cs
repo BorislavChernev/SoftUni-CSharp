51b72e8915b306b3edcbd2e640f96f0df3a8f5cf
BorislavChernev <borocool359@gmail.com>
2023-04-23 09:32:07 +0300

    Create Boardgame.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/Data/Models/Boardgame.cs
@@ -0,0 +1,44 @@
+﻿using Boardgames.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Boardgames.Data.Models;
+
+public class Boardgame
+{
+    public Boardgame()
+    {
+        this.BoardgamesSellers = new HashSet<BoardgameSeller>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(10)]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(1, 10.00)]
+    public double Rating { get; set; }
+
+    [Required]
+    [Range(2018, 2023)]
+    public int YearPublished { get; set; }
+
+    [Required]
+    public CategoryType CategoryType { get; set; }
+
+    [Required]
+    public string Mechanics { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Creator))]
+    public int CreatorId { get; set; }
+
+    [Required]
+    public Creator Creator { get; set; }
+
+    public ICollection<BoardgameSeller> BoardgamesSellers { get; set; }
+}
