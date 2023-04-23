6895491bcbc0ee3d98f9f89dc444f1943fb0b744
BorislavChernev <borocool359@gmail.com>
2023-04-23 09:32:20 +0300

    Create Seller.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/Data/Models/Seller.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models;
+
+public class Seller
+{
+    public Seller()
+    {
+        this.BoardgamesSellers = new HashSet<BoardgameSeller>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(30)]
+    public string Address { get; set; }
+
+    [Required]
+    public string Country { get; set; }
+
+    [Required]
+    [RegularExpression(@"^www.[a-zA-z\d\-]+.com$")]
+    public string Website { get; set; }
+
+    public ICollection<BoardgameSeller> BoardgamesSellers { get; set; }
+}
