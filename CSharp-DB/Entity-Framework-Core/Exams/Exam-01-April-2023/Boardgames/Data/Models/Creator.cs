e510d5624e03701e6e44a224d6cc8652fd4ecfcd
BorislavChernev <borocool359@gmail.com>
2023-04-23 09:32:14 +0300

    Create Creator.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/Data/Models/Creator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.Data.Models;
+
+public class Creator
+{
+    public Creator()
+    {
+        this.Boardgames = new HashSet<Boardgame>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(7)]
+    public string FirstName { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(7)]
+    public string LastName { get; set; }
+
+    public ICollection<Boardgame> Boardgames { get; set; }
+}
