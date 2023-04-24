8ab1649b274beeac7cf869d1abfe599b5ceedb77
BorislavChernev <borocool359@gmail.com>
2023-04-24 22:44:27 +0300

    Create ImportSellerJsonDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/DataProcessor/ImportDto/ImportSellerJsonDto.cs
@@ -0,0 +1,26 @@
+﻿using Boardgames.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Boardgames.DataProcessor.ImportDto;
+
+public class ImportSellerJsonDto
+{
+    [Required]
+    [MinLength(5)]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(30)]
+    public string Address { get; set; }
+
+    [Required]
+    public string Country { get; set; }
+
+    [Required]
+    [RegularExpression(@"^www.[a-zA-z\d\-]+.com$")]
+    public string Website { get; set; }
+
+    public int[] Boardgames { get; set; }
+}
