62790f214ce9efca21e3ba28d8176ce585af840d
BorislavChernev <borocool359@gmail.com>
2023-04-24 22:44:12 +0300

    Create ExportCreatorXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/DataProcessor/ExportDto/ExportCreatorXmlDto.cs
@@ -0,0 +1,17 @@
+﻿using Boardgames.Data.Models;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto;
+
+[XmlType("Creator")]
+public class ExportCreatorXmlDto
+{
+    [XmlElement("CreatorName")]
+    public string CreatorName { get; set; }
+
+    [XmlAttribute("BoardgamesCount")]
+    public int BoardgamesCount { get; set; }
+
+    [XmlArray("Boardgames")]
+    public ExportBoardgameXmlDto[] Boardgames { get; set; }
+}
