ddb073d2173bccf42c3837d3df39cefd40d69cc1
BorislavChernev <borocool359@gmail.com>
2023-04-24 22:44:19 +0300

    Create ImportCreatorXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/DataProcessor/ImportDto/ImportCreatorXmlDto.cs
@@ -0,0 +1,24 @@
+﻿using Boardgames.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ImportDto;
+
+[XmlType("Creator")]
+public class ImportCreatorXmlDto
+{
+    [XmlElement("FirstName")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(7)]
+    public string FirstName { get; set; }
+
+    [XmlElement("LastName")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(7)]
+    public string LastName { get; set; }
+
+    [XmlArray("Boardgames")]
+    public ImportBoardgameXmlDto[] Boardgames { get; set; }
+}
