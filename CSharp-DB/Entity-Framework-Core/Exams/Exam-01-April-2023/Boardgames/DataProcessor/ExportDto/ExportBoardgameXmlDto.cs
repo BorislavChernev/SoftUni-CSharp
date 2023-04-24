7143ac06687ba7dfebc524f55d938d4250193ec2
BorislavChernev <borocool359@gmail.com>
2023-04-24 22:44:09 +0300

    Create ExportBoardgameXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/DataProcessor/ExportDto/ExportBoardgameXmlDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Boardgames.DataProcessor.ExportDto;
+
+[XmlType("Boardgame")]
+public class ExportBoardgameXmlDto
+{
+    [XmlElement("BoardgameName")]
+    [Required]
+    [MinLength(10)]
+    [MaxLength(20)]
+    public string BoardgameName { get; set; }
+
+    [XmlElement("BoardgameYearPublished")]
+    [Required]
+    [Range(2018, 2023)]
+    public int BoardgameYearPublished { get; set; }
+}
