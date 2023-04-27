ae83b25a6138619ec93c893e024c582c79a59a4d
BorislavChernev <borocool359@gmail.com>
2023-04-27 22:43:29 +0300

    Create Employee.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models;
+
+public class Employee
+{
+    public Employee()
+    {
+        this.EmployeesTasks = new HashSet<EmployeeTask>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(40)]
+    public string Username { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\d{3}-\d{3}-\d{4}$")]
+    public string Phone { get; set; }
+
+    public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+}
