4aeeff7b92e367087755b125701362e292abde77
BorislavChernev <borocool359@gmail.com>
2023-04-27 22:43:11 +0300

    Create XmlHelper.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-01-April-2023/Boardgames/Utlities/XmlHelper.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace Boardgames.Utlities
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader reader = new StringReader(inputXml);
+            T deserializedDtos =
+                (T)xmlSerializer.Deserialize(reader);
+
+            return deserializedDtos;
+        }
+
+        public string Serialize<T>(T obj, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot =
+                new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T), xmlRoot);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, obj, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string Serialize<T>(T[] obj, string rootName)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            XmlRootAttribute xmlRoot =
+                new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer =
+                new XmlSerializer(typeof(T[]), xmlRoot);
+
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using StringWriter writer = new StringWriter(sb);
+            xmlSerializer.Serialize(writer, obj, namespaces);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
