df83b09559e66f675a560fa1f90fef179869b667
BorislavChernev <borocool359@gmail.com>
2023-04-29 09:32:14 +0300

    Create ExportProjectXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/DataProcessor/ExportDto/ExportProjectXmlDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto;
+
+[XmlType("Project")]
+public class ExportProjectXmlDto
+{
+    [XmlElement("ProjectName")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string ProjectName { get; set; }
+
+    [XmlElement("HasEndDate")]
+    public string HasEndDate { get; set; }
+
+    [XmlAttribute("TasksCount")]
+    public int TasksCount { get; set; }
+
+    [XmlArray("Tasks")]
+    public ExportTaskXmlDto[] Tasks { get; set; }
+}
