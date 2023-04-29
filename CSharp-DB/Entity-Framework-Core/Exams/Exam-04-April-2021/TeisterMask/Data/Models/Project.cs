f2a511698dea1f3bbefa037b4a7a257448a1f0b9
BorislavChernev <borocool359@gmail.com>
2023-04-29 09:32:02 +0300

    Create Project.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models;
+
+public class Project
+{
+    public Project()
+    {
+        this.Tasks = new HashSet<Task>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [Required]
+    public DateTime OpenDate { get; set; }
+
+    public DateTime? DueDate { get; set; }
+
+    public ICollection<Task> Tasks { get; set; }
+}
