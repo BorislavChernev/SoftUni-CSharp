15b630dbd644af78146fbad1f281521fb886a74c
BorislavChernev <borocool359@gmail.com>
2023-04-29 09:32:16 +0300

    Create ExportTaskXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/DataProcessor/ExportDto/ExportTaskXmlDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ExportDto;
+
+[XmlType("Task")]
+public class ExportTaskXmlDto
+{
+    [XmlElement("Name")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [XmlElement("Label")]
+    [Required]
+    public string Label { get; set; }
+}
