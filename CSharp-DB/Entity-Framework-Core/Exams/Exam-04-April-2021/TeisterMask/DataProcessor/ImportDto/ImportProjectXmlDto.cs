2528d2d65964c0319ae5d2b258196ab3e041a2ad
BorislavChernev <borocool359@gmail.com>
2023-05-01 22:07:33 +0300

    Create ImportProjectXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/DataProcessor/ImportDto/ImportProjectXmlDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto;
+
+[XmlType("Project")]
+public class ImportProjectXmlDto
+{
+    [XmlElement("Name")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [XmlElement("OpenDate")]
+    [Required]
+    public string OpenDate { get; set; }
+
+    [XmlElement("DueDate")]
+    public string? DueDate { get; set; }
+
+    [XmlArray("Tasks")]
+    public ImportTaskXmlDto[] Tasks { get; set; }
+}
