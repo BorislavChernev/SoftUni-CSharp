eaae9b160c918a1460ab80eca136f351995be0f9
BorislavChernev <borocool359@gmail.com>
2023-05-01 22:07:36 +0300

    Create ImportTaskXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-April-2021/TeisterMask/DataProcessor/ImportDto/ImportTaskXmlDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace TeisterMask.DataProcessor.ImportDto;
+
+[XmlType("Task")]
+public class ImportTaskXmlDto
+{
+    [XmlElement("Name")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [XmlElement("OpenDate")]
+    [Required]
+    public string OpenDate { get; set; }
+
+    [XmlElement("DueDate")]
+    [Required]
+    public string DueDate { get; set; }
+
+    [XmlElement("ExecutionType")]
+    [Required]
+    public int ExecutionType { get; set; }
+
+    [XmlElement("LabelType")]
+    [Required]
+    public int LabelType { get; set; }
+}
