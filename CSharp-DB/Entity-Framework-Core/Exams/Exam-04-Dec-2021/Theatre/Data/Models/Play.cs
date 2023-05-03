5afcb865675ac3be3b39f0d1ddc6a58d4668209e
BorislavChernev <borocool359@gmail.com>
2023-05-03 21:29:48 +0300

    Create Play.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/Data/Models/Play.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models;
+
+public class Play
+{
+    public Play()
+    {
+        this.Casts = new HashSet<Cast>();
+        this.Tickets = new HashSet<Ticket>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [Required]
+    public TimeSpan Duration { get; set; }
+
+    [Required]
+    [Range(0.00, 10.00)]
+    public float Rating { get; set; }
+
+    [Required]
+    public Genre Genre { get; set; }
+
+    [Required]
+    [MaxLength(700)]
+    public string Description { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Screenwriter { get; set; }
+
+    public ICollection<Cast> Casts { get; set; }
+
+    public ICollection<Ticket> Tickets { get; set; }
+}
