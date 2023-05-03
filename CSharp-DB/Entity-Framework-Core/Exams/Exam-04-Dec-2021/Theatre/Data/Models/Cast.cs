ad1da65d8eeb92d89afac95cc980dd90c096c616
BorislavChernev <borocool359@gmail.com>
2023-05-03 21:29:42 +0300

    Create Cast.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/Data/Models/Cast.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Theatre.Data.Models;
+
+public class Cast
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string FullName { get; set; }
+
+    [Required]
+    public bool IsMainCharacter { get; set; }
+
+    [Required]
+    [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+    public string PhoneNumber { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Play))]
+    public int PlayId { get; set; }
+
+    [Required]
+    public Play Play { get; set; }
+}
