a230b2ae6f663b0c5cb4174faf8f52d93aeef13e
BorislavChernev <borocool359@gmail.com>
2023-05-05 15:01:17 +0300

    Create Ticket.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/Data/Models/Ticket.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Theatre.Data.Models;
+
+public class Ticket
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Range(1.00, 100.00)]
+    public decimal Price { get; set; }
+
+    [Required]
+    [Range(1, 10)]
+    public sbyte RowNumber { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Play))]
+    public int PlayId { get; set; }
+
+    [Required]
+    public Play Play { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Theatre))]
+    public int TheatreId { get; set; }
+
+    [Required]
+    public Theatre Theatre { get; set; }
+}
