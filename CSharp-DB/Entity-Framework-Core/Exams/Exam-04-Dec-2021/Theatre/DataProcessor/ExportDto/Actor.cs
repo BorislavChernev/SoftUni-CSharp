5e33ff8ed1d990178113c6bc4a4a4962ec9ff915
BorislavChernev <borocool359@gmail.com>
2023-05-05 15:01:26 +0300

    Create Actor.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ExportDto/Actor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ExportDto;
+
+[XmlType("Actor")]
+public class Actor
+{
+    [XmlAttribute("FullName")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string FullName { get; set; }
+
+    [XmlAttribute("IsMainCharacter")]
+    [Required]
+    public string MainCharacter { get; set; }
+}
