80844be0371137b6ccc6338a6bbded6fea2ec99a
BorislavChernev <borocool359@gmail.com>
2023-05-05 15:01:14 +0300

    Create Theatre.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Theatre.Data.Models;
+
+public class Theatre
+{
+    public Theatre()
+    {
+        this.Tickets = new HashSet<Ticket>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(1, 10)]
+    public sbyte NumberOfHalls { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Director { get; set; }
+
+    public ICollection<Ticket> Tickets { get; set; }
+}
