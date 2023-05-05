693971aa476d65f34092cb0540369d3495066fec
BorislavChernev <borocool359@gmail.com>
2023-05-05 15:01:32 +0300

    Create ImportCastDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ImportDto/ImportCastDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models;
+using System.Xml.Serialization;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+[XmlType("Cast")]
+public class ImportCastDto
+{
+    [XmlElement("FullName")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string FullName { get; set; }
+
+    [XmlElement("IsMainCharacter")]
+    [Required]
+    public string IsMainCharacter { get; set; }
+
+    [XmlElement("PhoneNumber")]
+    [Required]
+    [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+    public string PhoneNumber { get; set; }
+
+    [XmlElement("PlayId")]
+    [Required]
+    [ForeignKey(nameof(Play))]
+    public int PlayId { get; set; }
+}
