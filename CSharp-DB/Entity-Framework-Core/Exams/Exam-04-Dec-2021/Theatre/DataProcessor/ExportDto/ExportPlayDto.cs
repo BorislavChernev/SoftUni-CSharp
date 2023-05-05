f022408fbc0b75a69c95958b0dc617bf27a23876
BorislavChernev <borocool359@gmail.com>
2023-05-05 15:01:28 +0300

    Create ExportPlayDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ExportDto/ExportPlayDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.DataProcessor.ExportDto;
+
+[XmlType("Play")]
+public class ExportPlayDto
+{
+    [XmlAttribute("Title")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [XmlAttribute("Duration")]
+    [Required]
+    public string Duration { get; set; }
+
+    [XmlAttribute("Rating")]
+    [Required]
+    public string Rating { get; set; }
+
+    [XmlAttribute("Genre")]
+    [Required]
+    public string Genre { get; set; }
+
+    [XmlArray("Actors")]
+    public Actor[] Actors { get; set; }
+}
