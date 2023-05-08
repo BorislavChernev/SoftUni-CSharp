0ab529e6998f1c8a4afb3d7a812caac818c9fe6b
BorislavChernev <borocool359@gmail.com>
2023-05-08 22:09:25 +0300

    Create ImportTicketDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ImportDto/ImportTicketDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+public class ImportTicketDto
+{
+    [Required]
+    [Range(1.00, 100.00)]
+    public decimal Price { get; set; }
+
+    [Required]
+    [Range(1, 10)]
+    public sbyte RowNumber { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Play))]
+    public int PlayId { get; set; }
+}
