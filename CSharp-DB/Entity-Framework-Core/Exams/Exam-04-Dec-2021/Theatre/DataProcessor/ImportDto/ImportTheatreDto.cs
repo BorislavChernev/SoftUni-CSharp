99797988320658da3cf311b23c0b58dbab8a7fb5
BorislavChernev <borocool359@gmail.com>
2023-05-08 22:09:22 +0300

    Create ImportTheatreDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ImportDto/ImportTheatreDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+public class ImportTheatreDto
+{
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Name { get; set; }
+
+    [Required]
+    [Range(1, 10)]
+    public sbyte NumberOfHalls { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Director { get; set; }
+
+    public ImportTicketDto[] Tickets { get; set; }
+}
