ec4bd1ab0d027528751f5fe753170413bed9d090
BorislavChernev <borocool359@gmail.com>
2023-05-08 22:09:18 +0300

    Create ImportPlayDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/DataProcessor/ImportDto/ImportPlayDto.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.DataProcessor.ImportDto;
+
+[XmlType("Play")]
+public class ImportPlayDto
+{
+    [XmlElement("Title")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(50)]
+    public string Title { get; set; }
+
+    [XmlElement("Duration")]
+    [Required]
+    public string Duration { get; set; }
+
+    [XmlElement("Raiting")]
+    [Required]
+    [Range(0.00, 10.00)]
+    public float Rating { get; set; }
+
+    [XmlElement("Genre")]
+    [Required]
+    public string Genre { get; set; }
+
+    [XmlElement("Description")]
+    [Required]
+    [MaxLength(700)]
+    public string Description { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Screenwriter { get; set; }
+}
