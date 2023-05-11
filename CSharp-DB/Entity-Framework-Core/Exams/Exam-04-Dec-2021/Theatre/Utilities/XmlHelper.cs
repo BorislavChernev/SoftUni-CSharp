33ad33731ee28bb2735d03f84b541430f13fd5b5
BorislavChernev <borocool359@gmail.com>
2023-05-11 20:18:38 +0300

    Create XmlHelper.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-04-Dec-2021/Theatre/Utilities/XmlHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace Trucks.Utilities;
+
+using System.Text;
+using System.Xml.Serialization;
+
+public class XmlHelper
+{
+    public T Deserialize<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T), xmlRoot);
+
+        using StringReader reader = new StringReader(inputXml);
+        T deserializedDtos =
+            (T)xmlSerializer.Deserialize(reader);
+
+        return deserializedDtos;
+    }
+
+    public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+    {
+        XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T[]), xmlRoot);
+
+        using StringReader reader = new StringReader(inputXml);
+        T[] supplierDtos =
+            (T[])xmlSerializer.Deserialize(reader);
+
+        return supplierDtos;
+    }
+
+    public string Serialize<T>(T obj, string rootName)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        XmlRootAttribute xmlRoot =
+            new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T), xmlRoot);
+
+        XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+        namespaces.Add(string.Empty, string.Empty);
+
+        using StringWriter writer = new StringWriter(sb);
+        xmlSerializer.Serialize(writer, obj, namespaces);
+
+        return sb.ToString().TrimEnd();
+    }
+
+    public string Serialize<T>(T[] obj, string rootName)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        XmlRootAttribute xmlRoot =
+            new XmlRootAttribute(rootName);
+        XmlSerializer xmlSerializer =
+            new XmlSerializer(typeof(T[]), xmlRoot);
+
+        XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+        namespaces.Add(string.Empty, string.Empty);
+
+        using StringWriter writer = new StringWriter(sb);
+        xmlSerializer.Serialize(writer, obj, namespaces);
+
+        return sb.ToString().TrimEnd();
+    }
+}
