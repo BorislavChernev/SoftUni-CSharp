a18c3fb4a677a6ea04c9f8cb41403020756f582a
BorislavChernev <borocool359@gmail.com>
2023-05-11 20:18:55 +0300

    Create Coach.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/Data/Models/Coach.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models;
+
+public class Coach
+{
+    public Coach()
+    {
+        this.Footballers = new HashSet<Footballer>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; } = null!;
+
+    public string Nationality { get; set; } = null!;
+
+    public ICollection<Footballer> Footballers { get; set; }
+}
