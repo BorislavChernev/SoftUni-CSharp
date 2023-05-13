0adb518c387028cfaae26ccc234f2f21793c87ad
BorislavChernev <borocool359@gmail.com>
2023-05-13 10:16:22 +0300

    Create TeamFootballer.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/Data/Models/TeamFootballer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models;
+
+public class TeamFootballer
+{
+    [ForeignKey(nameof(Team))]
+    public int TeamId { get; set; }
+
+    public Team Team { get; set; } = null!;
+
+    [ForeignKey(nameof(Footballer))]
+    public int FootballerId { get; set; }
+
+    public Footballer Footballer { get; set; } = null!;
+}
