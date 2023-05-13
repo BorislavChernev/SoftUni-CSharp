83e634e8b011a120d70c57b3f16b960c19ca240b
BorislavChernev <borocool359@gmail.com>
2023-05-13 10:16:28 +0300

    Create ExportCoachDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/DataProcessor/ExportDto/ExportCoachDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto;
+
+[XmlType("Coach")]
+public class ExportCoachDto
+{
+    [XmlElement("CoachName")]
+    public string Name { get; set; }
+
+    [XmlAttribute("FootballersCount")]
+    public int FootballersCount { get; set; }
+
+    [XmlArray("Footballers")]
+    public ExportFootballerDto[] Footballers { get; set; }
+}
