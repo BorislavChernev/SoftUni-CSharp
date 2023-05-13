5d93bb9693407b94e10cc5c7fbb106716ad48fa9
BorislavChernev <borocool359@gmail.com>
2023-05-13 10:16:18 +0300

    Create Footballer.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/Data/Models/Footballer.cs
@@ -0,0 +1,35 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models;
+
+public class Footballer
+{
+    public Footballer()
+    {
+        this.TeamsFootballers = new HashSet<TeamFootballer>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; } = null!;
+
+    public DateTime ContractStartDate { get; set; }
+
+    public DateTime ContractEndDate { get; set; }
+
+    public PositionType PositionType { get; set; }
+
+    public BestSkillType BestSkillType { get; set; }
+
+    [ForeignKey(nameof(Coach))]
+    public int CoachId { get; set; }
+
+    public Coach Coach { get; set; } = null!;
+
+    public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; }
+}
