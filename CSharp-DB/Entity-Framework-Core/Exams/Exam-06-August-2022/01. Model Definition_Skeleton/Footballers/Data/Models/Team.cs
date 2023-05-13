49bb00d027f069d7bd273571b9ba4d1d4f84c5cf
BorislavChernev <borocool359@gmail.com>
2023-05-13 10:16:20 +0300

    Create Team.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/Data/Models/Team.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Footballers.Data.Models;
+
+public class Team
+{
+    public Team()
+    {
+        this.TeamsFootballers = new HashSet<TeamFootballer>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [MinLength(3)]
+    [MaxLength(40)]
+    [RegularExpression(@"[A-Z][a-z][\s][\.][\-]")]
+    public string Name { get; set; } = null!;
+
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Nationality { get; set; } = null!;
+
+    public int Trophies { get; set; }
+
+    public virtual ICollection<TeamFootballer> TeamsFootballers { get; set; }
+}
