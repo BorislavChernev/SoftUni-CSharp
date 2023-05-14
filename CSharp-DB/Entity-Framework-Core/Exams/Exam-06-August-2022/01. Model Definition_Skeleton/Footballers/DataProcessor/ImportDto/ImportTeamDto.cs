3385e19257543e3477ac0f46ea7c226a6c1914eb
BorislavChernev <borocool359@gmail.com>
2023-05-14 19:42:11 +0300

    Create ImportTeamDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/DataProcessor/ImportDto/ImportTeamDto.cs
@@ -0,0 +1,20 @@
+﻿using Footballers.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+public class ImportTeamDto
+{
+    [MinLength(3)]
+    [MaxLength(40)]
+    [RegularExpression(@"^[A-Za-z0-9\s\.\-]{3,}$")]
+    public string Name { get; set; } = null!;
+
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Nationality { get; set; } = null!;
+
+    public int Trophies { get; set; }
+
+    public int[] Footballers { get; set; }
+}
