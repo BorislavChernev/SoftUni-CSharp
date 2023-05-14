3368e9d21301c8ab1ab41269df122f97705d0ce3
BorislavChernev <borocool359@gmail.com>
2023-05-14 19:42:06 +0300

    Create ImportCoachDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/DataProcessor/ImportDto/ImportCoachDto.cs
@@ -0,0 +1,19 @@
+﻿using Footballers.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+[XmlType("Coach")]
+public class ImportCoachDto
+{
+    [XmlElement("Name")]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; } = null!;
+    [XmlElement("Nationality")]
+    public string Nationality { get; set; } = null!;
+    
+    [XmlArray("Footballers")]
+    public ImportFootballerDto[] Footballers { get; set; }
+}
