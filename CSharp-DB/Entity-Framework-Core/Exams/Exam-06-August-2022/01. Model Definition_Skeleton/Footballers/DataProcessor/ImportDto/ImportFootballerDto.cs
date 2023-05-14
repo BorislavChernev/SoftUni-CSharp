67ed951b63d90557d200e0faab3e9d11152d41ae
BorislavChernev <borocool359@gmail.com>
2023-05-14 19:42:08 +0300

    Create ImportFootballerDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/DataProcessor/ImportDto/ImportFootballerDto.cs
@@ -0,0 +1,26 @@
+﻿using Footballers.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto;
+
+[XmlType("Footballer")]
+public class ImportFootballerDto
+{
+    [XmlElement("Name")]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; } = null!;
+
+    [XmlElement("ContractStartDate")]
+    public string ContractStartDate { get; set; }
+
+    [XmlElement("ContractEndDate")]
+    public string ContractEndDate { get; set; }
+
+    [XmlElement("BestSkillType")]
+    public int BestSkillType { get; set; }
+
+    [XmlElement("PositionType")]
+    public int PositionType { get; set; }
+}
