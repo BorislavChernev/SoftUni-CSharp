5d648537c847304c62f72337ae446c588f7e35be
BorislavChernev <borocool359@gmail.com>
2023-05-14 19:42:13 +0300

    Create Serializer.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/DataProcessor/Serializer.cs
@@ -0,0 +1,70 @@
+﻿namespace Footballers.DataProcessor
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using Footballers.DataProcessor.ExportDto;
+    using Footballers.Utilities;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportCoachesWithTheirFootballers(FootballersContext context)
+        {
+            IMapper mapper = CreateAutoMapper();
+
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportCoachDto[] coachDtos = context
+                .Coaches
+                .Where(c => c.Footballers.Any())
+                .ProjectTo<ExportCoachDto>(mapper.ConfigurationProvider)
+                .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.Name)
+                .ToArray();
+
+            return xmlHelper.Serialize(coachDtos, "Coaches");
+        }
+
+        public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
+        {
+            var teams = context
+                .Teams
+                .Where(t => t.TeamsFootballers.Any(tf => tf.Footballer.ContractStartDate >= date))
+                .Select(t => new
+                {
+                    t.Name,
+                    Footballers = t.TeamsFootballers
+                    .ToArray()
+                        .Where(tf => tf.Footballer.ContractStartDate >= date)
+                        .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                        .ThenBy(tf => tf.Footballer.Name)
+                        .Select(tf => new
+                        {
+                            FootballerName = tf.Footballer.Name,
+                            ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                            ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                            BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                            PositionType = tf.Footballer.PositionType.ToString()
+                        })
+                        .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Length)
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(teams, Formatting.Indented);
+        }
+
+        private static IMapper CreateAutoMapper()
+            => new Mapper(new MapperConfiguration(cfg =>
+                {
+                    cfg.AddProfile<FootballersProfile>();
+                }));
+    }
+}
