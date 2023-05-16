3906697ecb680935d9574bf9d9df4fdb51af3c93
BorislavChernev <borocool359@gmail.com>
2023-05-16 17:43:14 +0300

    Create FootballersProfile.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Exam-06-August-2022/01. Model Definition_Skeleton/Footballers/FootballersProfile.cs
@@ -0,0 +1,25 @@
+﻿namespace Footballers
+{
+    using System.Linq;
+
+    using AutoMapper;
+
+    using Data.Models;
+    using DataProcessor.ExportDto;
+
+
+    // Configure your AutoMapper here if you wish to use it. If not, DO NOT DELETE OR RENAME THIS CLASS
+    public class FootballersProfile : Profile
+    {
+        public FootballersProfile()
+        {
+            this.CreateMap<Footballer, ExportFootballerDto>()
+                .ForMember(dto => dto.Name, m => m.MapFrom(f => f.Name))
+                .ForMember(dto => dto.Position, m => m.MapFrom(f => f.PositionType.ToString()));
+            this.CreateMap<Coach, ExportCoachDto>()
+                .ForMember(dto => dto.Name, m => m.MapFrom(c => c.Name))
+                .ForMember(dto => dto.FootballersCount, m => m.MapFrom(c => c.Footballers.Count))
+                .ForMember(dto => dto.Footballers, m => m.MapFrom(c => c.Footballers.OrderBy(f => f.Name).ToArray()));
+        }
+    }
+}
