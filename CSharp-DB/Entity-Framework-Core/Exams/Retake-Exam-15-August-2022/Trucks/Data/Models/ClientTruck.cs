74d1bd7df0dbed52be52b6656f54a3c475702809
BorislavChernev <borocool359@gmail.com>
2023-05-16 17:43:33 +0300

    Create ClientTruck.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/Data/Models/ClientTruck.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Trucks.Data.Models;
+
+public class ClientTruck
+{
+    [ForeignKey(nameof(Client))]
+    public int ClientId { get; set; }
+
+    [Required]
+    public Client Client { get; set; }
+
+    [ForeignKey(nameof(Truck))]
+    public int TruckId { get; set; }
+
+    [Required]
+    public Truck Truck { get; set; }
+}
