bc38a3e7834d814281d94c3ee4d048220a86b277
BorislavChernev <borocool359@gmail.com>
2023-05-16 17:43:28 +0300

    Create Client.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/Data/Models/Client.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trucks.Data.Models;
+
+public class Client
+{
+    public Client()
+    {
+        this.ClientsTrucks = new HashSet<ClientTruck>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Nationality { get; set; }
+
+    [Required]
+    public string Type { get; set; }
+
+    public ICollection<ClientTruck> ClientsTrucks { get; set; }
+}
