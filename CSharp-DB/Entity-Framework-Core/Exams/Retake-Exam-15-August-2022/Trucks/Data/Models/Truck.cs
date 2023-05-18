74d1386a0d6e1baf9c70e98ee8d5cba797c9125a
Borislav Chernev <borocool359@gmail.com>
2023-05-19 01:52:24 +0300

    Create Truck.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/Data/Models/Truck.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.Data.Models;
+public class Truck
+{
+    public Truck()
+    {
+        this.ClientsTrucks = new HashSet<ClientTruck>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [RegularExpression(@"^[A-Z]{2}\d{4}[A-Z]{2}$")]
+    public string RegistrationNumber { get; set; }
+
+    [Required]
+    [StringLength(17)]
+    public string VinNumber { get; set; }
+
+    [Range(950, 1420)]
+    public int TankCapacity { get; set; }
+
+    [Range(5000, 29000)]
+    public int CargoCapacity { get; set; }
+
+    [Required]
+    public CategoryType CategoryType { get; set; }
+
+    [Required]
+    public MakeType MakeType { get; set; }
+
+    [Required]
+    [ForeignKey(nameof(Despatcher))]
+    public int DespatcherId { get; set; }
+
+    public Despatcher Despatcher { get; set; }
+
+    public ICollection<ClientTruck> ClientsTrucks { get; set; }
+}
