5abac3cc2e1c468bfbdf7cc0491fb31e641593fa
Borislav Chernev <borocool359@gmail.com>
2023-05-19 01:52:17 +0300

    Create Despatcher.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trucks.Data.Models;
+
+public class Despatcher
+{
+    public Despatcher()
+    {
+        this.Trucks = new HashSet<Truck>();
+    }
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    public string Position { get; set; }
+
+    public ICollection<Truck> Trucks { get; set; }
+}
