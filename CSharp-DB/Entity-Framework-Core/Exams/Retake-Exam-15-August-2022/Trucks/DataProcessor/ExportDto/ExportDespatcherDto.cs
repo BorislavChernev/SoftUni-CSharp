687f4c78e9fc8be7fc2b5943af1a92282f48a9b2
Borislav Chernev <borocool359@gmail.com>
2023-05-19 01:52:30 +0300

    Create ExportDespatcherDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ExportDto/ExportDespatcherDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Data.Models;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+public class ExportDespatcherDto
+{
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    public ExportTruckDto[] Trucks { get; set; }
+}
