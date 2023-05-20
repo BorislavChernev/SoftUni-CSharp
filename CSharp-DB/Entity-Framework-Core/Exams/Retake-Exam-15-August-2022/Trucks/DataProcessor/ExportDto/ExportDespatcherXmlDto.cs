c479290775def847c70bab0fa2f8d1c098843222
Borislav Chernev <borocool359@gmail.com>
2023-05-20 09:42:31 +0300

    Create ExportDespatcherXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ExportDto/ExportDespatcherXmlDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+[XmlType("Despatcher")]
+public class ExportDespatcherXmlDto
+{
+    [XmlElement("DespatcherName")]
+    public string DespatcherName { get; set; }
+
+    [XmlAttribute("TrucksCount")]
+    public int TrucksCount { get; set; }
+
+    [XmlArray("Trucks")]
+    public ExportTruckXmlDto[] Trucks { get; set; }
+}
