f96ef414023355f88131b6d074ce48c4c1b610cb
Borislav Chernev <borocool359@gmail.com>
2023-05-20 09:42:40 +0300

    Create ImportDespatcherDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ImportDto/ImportDespatcherDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Data.Models;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+[XmlType("Despatcher")]
+public class ImportDespatcherDto
+{
+    [XmlElement("Name")]
+    [Required]
+    [MinLength(2)]
+    [MaxLength(40)]
+    public string Name { get; set; }
+
+    [XmlElement("Position")]
+    public string Position { get; set; }
+
+    [XmlArray("Trucks")]
+    public ImportTruckDto[] Trucks { get; set; }
+}
