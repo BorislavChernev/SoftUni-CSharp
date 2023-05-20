8e3b162a24911731a513f93960b391f5473ad59c
Borislav Chernev <borocool359@gmail.com>
2023-05-20 09:42:43 +0300

    Create ImportTruckDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ImportDto/ImportTruckDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.DataProcessor.ImportDto;
+
+[XmlType("Truck")]
+public class ImportTruckDto
+{
+    [XmlElement("RegistrationNumber")]
+    [RegularExpression(@"^[A-Z]{2}\d{4}[A-Z]{2}$")]
+    public string RegistrationNumber { get; set; }
+
+    [XmlElement("VinNumber")]
+    [Required]
+    [MinLength(17)]
+    [MaxLength(17)]
+    public string VinNumber { get; set; }
+
+    [XmlElement("TankCapacity")]
+    [Range(950, 1420)]
+    public int TankCapacity { get; set; }
+
+    [XmlElement("CargoCapacity")]
+    [Range(5000, 29000)]
+    public int CargoCapacity { get; set; }
+
+    [XmlElement("CategoryType")]
+    [Required]
+    public int CategoryType { get; set; }
+
+    [XmlElement("MakeType")]
+    [Required]
+    public int MakeType { get; set; }
+}
