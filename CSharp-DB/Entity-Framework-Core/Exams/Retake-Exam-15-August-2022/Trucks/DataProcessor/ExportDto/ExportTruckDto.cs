09d3634f41619d989500899b20453e4c59c6d0b9
Borislav Chernev <borocool359@gmail.com>
2023-05-20 09:42:33 +0300

    Create ExportTruckDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ExportDto/ExportTruckDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+public class ExportTruckDto
+{
+    [RegularExpression(@"^[A-Z]{2}\d{4}[A-Z]{2}$")]
+    public string TruckRegistrationNumber { get; set; }
+
+    [Required]
+    [StringLength(17)]
+    public string VinNumber { get; set; }
+
+    [Range(950, 1420)]
+    public int TankCapacity { get; set; }
+
+    [Range(5000, 29000)]
+    public int CargoCapacity { get; set; }
+
+    [Required]
+    public string CategoryType { get; set; }
+
+    [Required]
+    public string MakeType { get; set; }
+}
