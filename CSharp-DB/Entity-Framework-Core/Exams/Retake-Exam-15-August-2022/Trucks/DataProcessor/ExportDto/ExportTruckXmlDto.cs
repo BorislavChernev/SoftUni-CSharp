71ba92d3dd412c397717ecb21d961ea18bb52c1b
Borislav Chernev <borocool359@gmail.com>
2023-05-20 09:42:36 +0300

    Create ExportTruckXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/DataProcessor/ExportDto/ExportTruckXmlDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto;
+
+[XmlType("Truck")]
+public class ExportTruckXmlDto
+{
+    [XmlElement("RegistrationNumber")]
+    public string RegistrationNumber { get; set; }
+
+    [XmlElement("Make")]
+    public string MakeType { get; set; }
+}
