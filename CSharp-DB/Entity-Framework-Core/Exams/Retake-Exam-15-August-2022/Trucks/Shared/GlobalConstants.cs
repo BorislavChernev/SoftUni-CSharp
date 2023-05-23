7b912aeab872d02ac46c44a385bdfa7dd50d36a6
Borislav Chernev <borocool359@gmail.com>
2023-05-23 23:02:32 +0300

    Create GlobalConstants.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-15-August-2022/Trucks/Shared/GlobalConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Trucks.Shared
+{
+    public static class GlobalConstants
+    {
+        //Client
+        public const int ClientNameMinLength = 3;
+        public const int ClientNameMaxLength = 40;
+        public const int ClientNationalityMinLength = 2;
+        public const int ClientNationalityMaxLength = 40;
+
+
+        //Despatcher
+        public const int DespatcherNameMinLength = 2;
+        public const int DespatcherNameMaxLength = 40;
+
+
+        //Truck
+        public const int TruckVinNumberLength = 17;
+        public const string TruckRegistrationNumberRegex = @"[A-Z]{2}[0-9]{4}[A-Z]{2}$";
+    }
+}
