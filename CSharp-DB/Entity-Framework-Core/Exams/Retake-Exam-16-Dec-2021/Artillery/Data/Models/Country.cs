e4f457cb834fc5d2232bd9705231c98d61974b01
Borislav Chernev <borocool359@gmail.com>
2023-05-25 23:20:53 +0300

    Create Country.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/Data/Models/Country.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models;
+
+public class Country
+{
+    public Country()
+    {
+        this.CountriesGuns = new HashSet<CountryGun>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(60)]
+    public string CountryName { get; set; }
+
+    [Required]
+    [Range(50_000, 10_000_000)]
+    public int ArmySize { get; set; }
+
+    public ICollection<CountryGun> CountriesGuns { get; set; }
+}
