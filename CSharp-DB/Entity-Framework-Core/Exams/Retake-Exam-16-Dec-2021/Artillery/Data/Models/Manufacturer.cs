d2706557a7dd13e9b8aa7f18a9b7a6e4a2ee58b9
Borislav Chernev <borocool359@gmail.com>
2023-05-28 20:30:32 +0300

    Create Manufacturer.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/Data/Models/Manufacturer.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models;
+
+public class Manufacturer
+{
+    public Manufacturer()
+    {
+        this.Guns = new HashSet<Gun>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(40)]
+    public string ManufacturerName { get; set; }
+
+    [Required]
+    [MinLength(10)]
+    [MaxLength(100)]
+    public string Founded { get; set; }
+
+    public ICollection<Gun> Guns { get; set; }
+}
