c08ca47fc1503bfcfe8b256eb25840c0e0ddd04e
Borislav Chernev <borocool359@gmail.com>
2023-05-28 20:30:34 +0300

    Create Shell.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models;
+
+public class Shell
+{
+    public Shell()
+    {
+        this.Guns = new HashSet<Gun>();
+    }
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Range(2, 1_680)]
+    public double ShellWeight { get; set; }
+
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Caliber { get; set; }
+
+    public ICollection<Gun> Guns { get; set; }
+}
