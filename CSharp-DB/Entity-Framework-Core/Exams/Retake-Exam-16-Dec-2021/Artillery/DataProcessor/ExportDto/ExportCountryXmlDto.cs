66b84cff6e3cb162d99824df7adf2610178332c5
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:13 +0300

    Create ExportCountryXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ExportDto/ExportCountryXmlDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto;
+
+[XmlType("Country")]
+public class ExportCountryXmlDto
+{
+    [XmlAttribute("CountryName")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(60)]
+    public string CountryName { get; set; }
+
+    [XmlAttribute("ArmySize")]
+    [Required]
+    [Range(50_000, 10_000_000)]
+    public int ArmySize { get; set; }
+}
