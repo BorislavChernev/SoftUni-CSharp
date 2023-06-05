d669cdce5f4c2d6930a05dbcd416ee41556fe3af
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:19 +0300

    Create ImportCountryDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ImportDto/ImportCountryDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Country")]
+public class ImportCountryDto
+{
+    [XmlElement("CountryName")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(60)]
+    public string CountryName { get; set; }
+
+    [XmlElement("ArmySize")]
+    [Required]
+    [Range(50_000, 10_000_000)]
+    public int ArmySize { get; set; }
+}
