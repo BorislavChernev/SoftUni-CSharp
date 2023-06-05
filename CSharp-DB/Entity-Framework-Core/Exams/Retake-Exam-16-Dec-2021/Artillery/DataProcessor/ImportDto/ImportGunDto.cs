1fb07c12970db8288ac3275f9a1f7ee1cfd58a77
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:25 +0300

    Create ImportGunDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ImportDto/ImportGunDto.cs
@@ -0,0 +1,34 @@
+﻿using Artillery.Data.Models.Enums;
+using Artillery.Data.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+public class ImportGunDto
+{
+    [Required]
+    public int ManufacturerId { get; set; }
+
+    [Required]
+    [Range(100, 1_350_000)]
+    public int GunWeight { get; set; }
+
+    [Required]
+    [Range(2.00, 35.00)]
+    public double BarrelLength { get; set; }
+
+    public int? NumberBuild { get; set; }
+
+    [Required]
+    [Range(1, 100_000)]
+    public int Range { get; set; }
+
+    [Required]
+    public string GunType { get; set; }
+
+    [Required]
+    public int ShellId { get; set; }
+
+    public ImportCountryGunDto[] Countries { get; set; }
+}
