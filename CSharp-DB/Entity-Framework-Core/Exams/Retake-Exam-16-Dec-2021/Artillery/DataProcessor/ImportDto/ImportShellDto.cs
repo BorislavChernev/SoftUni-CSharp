d7d3e6bc203711ff3595b6b8c2d743c707442514
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:29 +0300

    Create ImportShellDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ImportDto/ImportShellDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Shell")]
+public class ImportShellDto
+{
+    [XmlElement("ShellWeight")]
+    [Required]
+    [Range(2, 1_680)]
+    public double ShellWeight { get; set; }
+
+    [XmlElement("Caliber")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(30)]
+    public string Caliber { get; set; }
+}
