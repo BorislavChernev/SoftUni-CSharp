4c915e7198c8f9f62d2986777169e747d9da3b5f
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:16 +0300

    Create ExportGunXmlDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ExportDto/ExportGunXmlDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ExportDto;
+
+[XmlType("Gun")]
+public class ExportGunXmlDto
+{
+    [XmlAttribute("Manufacturer")]
+    [Required]
+    public string Manufacturer { get; set; }
+
+    [XmlAttribute("GunType")]
+    [Required]
+    public string GunType { get; set; }
+
+    [XmlAttribute("GunWeight")]
+    [Required]
+    [MinLength(100)]
+    [MaxLength(1_350_000)]
+    public int GunWeight { get; set; }
+
+    [XmlAttribute("BarrelLength")]
+    [Required]
+    [Range(2.00, 35.00)]
+    public double BarrelLength { get; set; }
+
+    [XmlAttribute("Range")]
+    [Required]
+    [Range(1, 100_000)]
+    public int Range { get; set; }
+
+    [XmlArray("Countries")]
+    public ExportCountryXmlDto[] Countries { get; set; }
+}
