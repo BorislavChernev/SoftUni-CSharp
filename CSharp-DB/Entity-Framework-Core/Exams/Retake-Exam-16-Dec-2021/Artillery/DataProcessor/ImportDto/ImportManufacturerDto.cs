c194fa1593571847f26377d5409c7e690a0adeff
Borislav Chernev <borocool359@gmail.com>
2023-06-05 21:12:27 +0300

    Create ImportManufacturerDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Exams/Retake-Exam-16-Dec-2021/Artillery/DataProcessor/ImportDto/ImportManufacturerDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto;
+
+[XmlType("Manufacturer")]
+public class ImportManufacturerDto
+{
+    [XmlElement("ManufacturerName")]
+    [Required]
+    [MinLength(4)]
+    [MaxLength(40)]
+    public string ManufacturerName { get; set; }
+
+    [XmlElement("Founded")]
+    [Required]
+    [MinLength(10)]
+    [MaxLength(100)]
+    public string Founded { get; set; }
+}
