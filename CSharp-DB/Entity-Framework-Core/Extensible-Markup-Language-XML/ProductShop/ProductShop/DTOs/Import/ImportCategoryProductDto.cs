c6cb3f2b58b4a436ecc9ba776ff28c4ac79b4a66
Borislav Chernev <borocool359@gmail.com>
2023-06-19 21:46:47 +0300

    Create ImportCategoryProductDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/DTOs/Import/ImportCategoryProductDto.cs
@@ -0,0 +1,16 @@
+﻿using ProductShop.Models;
+using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import;
+
+[XmlType("CategoryProduct")]
+public class ImportCategoryProductDto
+{
+    [XmlElement("CategoryId")]
+    public int CategoryId { get; set; }
+    [XmlElement("ProductId")]
+    public int ProductId { get; set; }
+    //<CategoryProduct>
+    //    <CategoryId>4</CategoryId>
+    //    <ProductId>1</ProductId>
+}
