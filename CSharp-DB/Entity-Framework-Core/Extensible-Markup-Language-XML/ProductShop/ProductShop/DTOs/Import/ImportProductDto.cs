8c4ca9d7cd0ba0f9d440a8975602489e4e700e42
Borislav Chernev <borocool359@gmail.com>
2023-06-19 21:46:50 +0300

    Create ImportProductDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/DTOs/Import/ImportProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import;
+
+[XmlType("Product")]
+public class ImportProductDto
+{
+    [XmlElement("name")]
+    public string Name { get; set; } = null!;
+    [XmlElement("price")]
+    public decimal Price { get; set; }
+    [XmlElement("sellerId")]
+    public int SellerId { get; set; }
+    [XmlElement("buyerId")]
+    public int? BuyerId { get; set; }
+}
