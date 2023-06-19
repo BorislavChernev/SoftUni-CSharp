3a045959ee9fb0024f72bee0367e702d22709234
Borislav Chernev <borocool359@gmail.com>
2023-06-19 21:46:39 +0300

    Create ExportProductDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/DTOs/Export/ExportProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Export;
+
+[XmlType("Product")]
+public class ExportProductDto
+{
+    [XmlElement("name")]
+    public string Name { get; set; } = null!;
+    [XmlElement("price")]
+    public decimal Price { get; set; }
+    [XmlElement("buyer")]
+    public string Buyer { get; set; }
+}
