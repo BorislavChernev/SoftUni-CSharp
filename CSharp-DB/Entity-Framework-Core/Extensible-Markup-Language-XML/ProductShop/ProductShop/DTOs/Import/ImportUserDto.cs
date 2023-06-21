1da0b8194bea33bda366a46259cec72edb832c1f
Borislav Chernev <borocool359@gmail.com>
2023-06-21 14:31:53 +0300

    Create ImportUserDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/DTOs/Import/ImportUserDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import;
+
+[XmlType("User")]
+public class ImportUserDto
+{
+    [XmlElement("firstName")]
+    public string FirstName { get; set; } = null!;
+    [XmlElement("lastName")]
+    public string LastName { get; set; } = null!;
+    [XmlElement("age")]
+    public int? Age { get; set; }
+}
