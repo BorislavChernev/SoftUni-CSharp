b50a10ee5f6d635b127aad9fc64e842f7b2495b8
Borislav Chernev <borocool359@gmail.com>
2023-06-22 23:51:04 +0300

    Create ProductShopProfile.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/ProductShopProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ProductShop.DTOs.Export;
+using ProductShop.Models;
+
+namespace ProductShop;
+
+public class ProductShopProfile : Profile
+{
+    public ProductShopProfile()
+    {
+        this.CreateMap<Product, ExportProductDto>()
+            .ForMember(d => d.Buyer,
+                opt => opt.MapFrom(s => $"{s.Buyer.FirstName} {s.Buyer.LastName}"));
+
+        this.CreateMap<User, ExportSoldProductsDto>();
+        this.CreateMap<User, ExportSoldProductsDto>()
+            .ForMember(d => d.SoldProducts,
+                opt => opt.MapFrom(s =>
+                    s.ProductsSold
+                        .Select(pc => new ExportProductDto()
+                        {
+                            Name = pc.Name,
+                            Price = pc.Price
+                        })
+                        .ToArray()));
+    }
+}
