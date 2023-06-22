0aaead447918deb8b734696921b15a838bbc682f
Borislav Chernev <borocool359@gmail.com>
2023-06-22 23:51:17 +0300

    Create StringWriterWithEncoding.cs

---
+++ CSharp-DB/Entity-Framework-Core/Extensible-Markup-Language-XML/ProductShop/ProductShop/Utilities/StringWriterWithEncoding.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ProductShop.Utilities;
+
+public sealed class StringWriterWithEncoding : StringWriter
+{
+    private readonly Encoding encoding;
+
+    public StringWriterWithEncoding() { }
+
+    public StringWriterWithEncoding(Encoding encoding)
+    {
+        this.encoding = encoding;
+    }
+
+    public override Encoding Encoding
+    {
+        get { return encoding; }
+    }
+}
+
