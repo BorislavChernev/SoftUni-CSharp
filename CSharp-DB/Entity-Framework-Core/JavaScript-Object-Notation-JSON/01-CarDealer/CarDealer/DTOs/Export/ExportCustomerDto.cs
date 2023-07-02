73a09fda3261e4484615fbf5847100a1b34ca3f7
Borislav Chernev <borocool359@gmail.com>
2023-07-02 22:48:10 +0300

    Create ExportCustomerDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-CarDealer/CarDealer/DTOs/Export/ExportCustomerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarDealer.DTOs.Export;
+
+public class ExportCustomerDto
+{
+    public string Name { get; set; } = null!;
+    public DateTime BirthDate { get; set; }
+    public bool IsYoungDriver { get; set; }
+}
