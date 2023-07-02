7926bf76c0eac30429e001acf3848e9b59da0ccc
Borislav Chernev <borocool359@gmail.com>
2023-07-02 22:48:12 +0300

    Create ImportCarDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-CarDealer/CarDealer/DTOs/Import/ImportCarDto.cs
@@ -0,0 +1,16 @@
+﻿using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Import;
+
+public class ImportCarDto
+{
+    [JsonProperty("make")]
+    public string Make { get; set; } = null!;
+    [JsonProperty("model")]
+    public string Model { get; set; } = null!;
+    [JsonProperty("traveledDistance")]
+    public long TravelledDistance { get; set; }
+    [JsonProperty("partsId")]
+    public ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+}
