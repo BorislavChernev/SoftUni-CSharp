9ef57684674e6d562ff66e7ab278981542f0e71d
Borislav Chernev <borocool359@gmail.com>
2023-07-10 13:51:38 +0300

    Create ImportSupplierDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-CarDealer/CarDealer/DTOs/Import/ImportSupplierDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CarDealer.DTOs.Import;
+
+using Newtonsoft.Json;
+
+public class ImportSupplierDto
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+
+    [JsonProperty("isImporter")]
+    public bool IsImporter { get; set; }
+}
