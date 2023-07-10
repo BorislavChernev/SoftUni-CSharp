3c9b75a84048c6d1d382b6c80c058fe2f0215d82
Borislav Chernev <borocool359@gmail.com>
2023-07-10 13:51:28 +0300

    Create ImportCustomerDto.cs

---
+++ CSharp-DB/Entity-Framework-Core/JavaScript-Object-Notation-JSON/01-CarDealer/CarDealer/DTOs/Import/ImportCustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer.DTOs.Import;
+
+public class ImportCustomerDto
+{
+
+    //"name": "Marcelle Griego",
+    //"birthDate": "1990-10-04T00:00:00",
+    //"isYoungDriver": true
+    [JsonProperty("name")]
+    public string Name { get; set; } = null!;
+    [JsonProperty("birthDate")]
+    public DateTime BirthDate { get; set; }
+    [JsonProperty("isYoungDriver")]
+    public bool IsYoungDriver { get; set; }
+}
