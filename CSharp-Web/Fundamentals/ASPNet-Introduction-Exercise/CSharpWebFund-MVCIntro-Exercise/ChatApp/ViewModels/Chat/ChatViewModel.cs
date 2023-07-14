3ed609487a66b2406caceceb4c216a223977fd96
Borislav Chernev <borocool359@gmail.com>
2023-07-14 09:40:44 +0300

    Create ChatViewModel.cs

---
+++ CSharp-Web/Fundamentals/ASPNet-Introduction-Exercise/CSharpWebFund-MVCIntro-Exercise/ChatApp/ViewModels/Chat/ChatViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ChatApp.Models.Chat
+{
+    public class ChatViewModel
+    {
+        public ChatViewModel()
+        {
+            this.AllMesages = new HashSet<MessageViewModel>();
+        }
+
+        public MessageViewModel CurrentMessage { get; set; }
+
+        public ICollection<MessageViewModel> AllMesages { get; set; }
+    }
+}
