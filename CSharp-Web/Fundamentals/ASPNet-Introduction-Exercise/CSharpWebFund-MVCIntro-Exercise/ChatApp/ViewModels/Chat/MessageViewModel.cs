5e99bbabd05bd929959d50b8b23d152a738374a6
Borislav Chernev <borocool359@gmail.com>
2023-07-14 09:40:47 +0300

    Create MessageViewModel.cs

---
+++ CSharp-Web/Fundamentals/ASPNet-Introduction-Exercise/CSharpWebFund-MVCIntro-Exercise/ChatApp/ViewModels/Chat/MessageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatApp.Models.Chat
+{
+    public class MessageViewModel
+    {
+        [Required]
+        public string Sender { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string MessageText { get; set; }
+    }
+}
